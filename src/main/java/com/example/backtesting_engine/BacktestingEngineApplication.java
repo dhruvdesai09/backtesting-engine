10debd376774eb96b3806325f7297fc1cd8c9f1e
Dhruv Desai <desaidhruv2004@gmail.com>
2026-01-23 02:22:18 +0530

    start

---
+++ src/main/java/com/example/backtesting_engine/BacktestingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.backtesting_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacktestingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacktestingEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backtesting_engine/BacktestingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backtesting_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacktestingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
